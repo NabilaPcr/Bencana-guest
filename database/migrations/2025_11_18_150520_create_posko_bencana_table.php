02522ba1ec696828c997e408b427bc1579d885a5
NabilaPcr <nabila24si@mahasiswa.pcr.ac.id>
2025-11-20 10:51:13 +0700

    perubahan lagi

--- database/migrations/2025_11_18_150520_create_posko_bencana_table.php
+++ database/migrations/2025_11_18_150520_create_posko_bencana_table.php
@@ -4,25 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
 
 class PoskoBencanaSeeder extends Seeder
 {
     public function run(): void
     {
-        $faker = Faker::create('id_ID');
-
-        // Misalkan kamu punya 10 posko untuk beberapa kejadian
-        for ($i = 1; $i <= 10; $i++) {
-            DB::table('posko_bencana')->insert([
-                'kejadian_id' => $faker->numberBetween(1, 10), // sesuaikan jumlah kejadian yang ada
-                'nama' => 'Posko ' . $faker->city,
-                'alamat' => $faker->address,
-                'kontak' => $faker->phoneNumber,
-                'penanggung_jawab' => $faker->name,
+        DB::table('posko_bencana')->insert([
+            [
+                'kejadian_id' => 4,
+                'nama' => 'Posko Ville',
+                'alamat' => 'Dk. R.M. Said No. 421, Tegal 62509, Kaltim',
+                'kontak' => '08147605806',
+                'penanggung_jawab' => 'Gasti Suryatmi',
                 'created_at' => now(),
                 'updated_at' => now(),
-            ]);
-        }
+            ]
+        ]);
     }
 }
