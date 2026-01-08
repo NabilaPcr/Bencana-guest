4753713066030eba281207dea9a7d3027962ef0f
NabilaPcr <nabila24si@mahasiswa.pcr.ac.id>
2026-01-08 22:58:45 +0700

    PERUBAHAN KECIK

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 
     public function index()
     {
-        return view('pages.auth.formlogin');
+        return view('pages.auth.formLogin');
     }
 
     // Proses login
